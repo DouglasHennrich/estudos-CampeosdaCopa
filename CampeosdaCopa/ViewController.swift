d08b8dc082715fbdbcf6777c84624963b3658f80
Douglas Hennrich <douglas_b_h@hotmail.com>
2020-08-07 11:33:54 -0300

    Initial Commit

---
+++ CampeosdaCopa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampeosdaCopa
+//
+//  Created by Douglas Hennrich on 07/08/20.
+//  Copyright © 2020 Douglas Hennrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
